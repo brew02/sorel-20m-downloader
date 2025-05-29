12104052bb17309196c9b2bd95e58c82696812cc
Brodie Abrew <brodieabrew@u.boisestate.edu>
2025-05-29 16:22:21 -0600

    Finish basic implementation of the script

--- sorel-20m-downloader.py
+++ sorel-20m-downloader.py
@@ -1,43 +1,69 @@
 import xml.etree.ElementTree as ET
 import os
-import time
 import requests
 import boto3
 from botocore import UNSIGNED
 from botocore.config import Config
 
+# Open a new AWS S3 boto3 client without authentication
 s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
-
-prefix = "09-DEC-2020/binaries/"
-prefix_len = len(prefix)
-params = {"prefix": prefix}
-sorel_xml = requests.get("http://sorel-20m.s3.amazonaws.com/", params=params)
+# Make the bin directory if it doesn't exist
 os.makedirs("bin", exist_ok=True)
 
-downloaded = 0
-limit = 3000000 # Set limit to 3MB
-sorel_20m = {"sorel_20m": "http://s3.amazonaws.com/doc/2006-03-01/"}
+prefix = "09-DEC-2020/binaries/" # Prefix for keys that represent binaries
+prefix_len = len(prefix)
 
-root = ET.fromstring(sorel_xml.text)
+# Use the prefix in the URL parameters. This way
+# we only get objects that represent binaries
+params = {"prefix": prefix}
 
-# is_truncated = True
-# while is_truncated:
+num_down = 0
+bytes_down = 0
+total_bin_size = 0
+limit = 30000000 # Set binary download limit to 30MB
+namespace = {"namespace": "http://s3.amazonaws.com/doc/2006-03-01/"} # xmlns namespace
 
-#     is_truncated = root.find("sorel_20m:IsTruncated", sorel_20m).text == "True"
+while True:
+    # Download the XML file from AWS (you can view this XML file by
+    # pasting the URL into your browser)
+    sorel_xml = requests.get("http://sorel-20m.s3.amazonaws.com/", params=params)
+    # Create the root from the XML file
+    root = ET.fromstring(sorel_xml.text)
 
-# TODO: To get more binaries, use the last key returned in the
-# XML as the marker parameter in the URL request
-for contents in root.findall("sorel_20m:Contents", sorel_20m):
-    key = contents.find("sorel_20m:Key", sorel_20m)
+    done = False
 
-    size = int(contents.find("sorel_20m:Size", sorel_20m).text)
-    if (downloaded + size) > limit:
+    # For each Contents element from the root
+    for contents in root.findall("namespace:Contents", namespace):
+        # Get the Key element
+        key = contents.find("namespace:Key", namespace)
+        # Update the marker in case the XML was truncated
+        params["marker"] = key
+
+        # Get the binary size
+        size = int(contents.find("namespace:Size", namespace).text)
+        if (total_bin_size + size) > limit:
+            done = True
+            break
+        
+        # Get the name of the binary
+        binary_name = key.text[prefix_len:]
+        
+        # If the binary hasn't been downloaded yet, download it
+        if os.path.exists("bin/" + binary_name) == False:
+            s3.download_file("sorel-20m", prefix + binary_name, "bin/" + binary_name)
+            bytes_down = bytes_down + size
+            num_down = num_down + 1
+
+        total_bin_size = total_bin_size + size
+
+    if done:
         break
-    
-    binary_name = key.text[prefix_len:]
-    if os.path.exists("bin/" + binary_name) == False:
-        s3.download_file("sorel-20m", prefix + binary_name, "bin/" + binary_name)
 
-    downloaded = downloaded + size
+    # If the data was truncated and we aren't done,
+    # request the next piece of data, otherwise break
+    is_truncated = root.find("namespace:IsTruncated", namespace).text == "True"
+    if is_truncated == False:
+        break
 
-print(downloaded)
+# Print summary
+print("Downloaded " + str(num_down) + " binaries: " + str(bytes_down) + " bytes")
